5365a863d20ce095c91305900c1b83cd8d71aa97
Stephen Belanger <stephen.belanger@elastic.co>
2019-04-02 10:51:25 +0200

    Add coverage testing

--- test/test.js
+++ test/test.js
@@ -7,6 +7,10 @@ const tap = require('tap')
 
 const redMaple = require('../')
 
+const numbers = [1, 2, 3]
+
+function noop () {}
+
 tap.test('map', t => {
   t.plan(1)
 
@@ -14,13 +18,13 @@ tap.test('map', t => {
     return item.value * 2
   }
 
-  const inputs = [1,2,3].map(value => ({ value }))
+  const inputs = numbers.map(value => ({ value }))
   const output = inputs.map(map).join('\n')
 
   redMaple({
     input: intoStream(inputs.map(v => JSON.stringify(v)).join('\n')),
     output: concat(result => {
-      t.equal(result.toString(), output + "\n", 'received expected output')
+      t.equal(result.toString(), `${output}\n`, 'received expected output')
     }),
     map
   })
@@ -37,13 +41,13 @@ tap.test('map + reduce', t => {
     return (memo || 0) + item
   }
 
-  const inputs = [1,2,3].map(value => ({ value }))
+  const inputs = numbers.map(value => ({ value }))
   const output = inputs.map(map).reduce(reduce)
 
   redMaple({
     input: intoStream(inputs.map(v => JSON.stringify(v)).join('\n')),
     output: concat(result => {
-      t.equal(result.toString(), output + "\n", 'received expected output')
+      t.equal(result.toString(), `${output}\n`, 'received expected output')
     }),
     map,
     reduce
@@ -56,13 +60,13 @@ tap.test('expression map + reduce', t => {
   const map = ensureFunction('item.value * 2', ['item'])
   const reduce = ensureFunction('(memo || 0) + item', ['memo', 'item'])
 
-  const inputs = [1,2,3].map(value => ({ value }))
+  const inputs = numbers.map(value => ({ value }))
   const output = inputs.map(map).reduce(reduce)
 
   redMaple({
     input: intoStream(inputs.map(v => JSON.stringify(v)).join('\n')),
     output: concat(result => {
-      t.equal(result.toString(), output + "\n", 'received expected output')
+      t.equal(result.toString(), `${output}\n`, 'received expected output')
     }),
     map,
     reduce
@@ -80,13 +84,13 @@ tap.test('initial reduce value', t => {
     return (memo || 0) + item
   }
 
-  const inputs = [1,2,3].map(value => ({ value }))
+  const inputs = numbers.map(value => ({ value }))
   const output = inputs.map(map).reduce(reduce)
 
   redMaple({
     input: intoStream(inputs.map(v => JSON.stringify(v)).join('\n')),
     output: concat(result => {
-      t.equal(result.toString(), (output + 2) + "\n", 'received expected output')
+      t.equal(result.toString(), `${(output + 2)}\n`, 'received expected output')
     }),
     map,
     reduce,
@@ -105,13 +109,13 @@ tap.test('custom parser + serializer', t => {
     return (memo || 0) + item
   }
 
-  const inputs = [1,2,3].map(value => ({ value }))
+  const inputs = numbers.map(value => ({ value }))
   const output = inputs.map(map).reduce(reduce)
 
   let parsed = 0
   const parser = new Transform({
     objectMode: true,
-    transform(chunk, encoding, callback) {
+    transform (chunk, encoding, callback) {
       parsed++
       callback(null, chunk, encoding)
     }
@@ -120,15 +124,15 @@ tap.test('custom parser + serializer', t => {
   let serialized = 0
   const serializer = new Transform({
     objectMode: true,
-    transform(chunk, encoding, callback) {
+    transform (chunk, encoding, callback) {
       serialized++
       callback(null, chunk, encoding)
     }
   })
-  
+
   const outputStream = new Writable({
     objectMode: true,
-    write(chunk, encoding, callback) {
+    write (chunk, encoding, callback) {
       t.equal(chunk, output, 'output matches')
       t.equal(parsed, 3, 'parsed expected number of items')
       t.equal(serialized, 1, 'serialized expected number of items')
@@ -144,4 +148,29 @@ tap.test('custom parser + serializer', t => {
     map,
     reduce
   })
+})
+
+tap.test('reject invalid input stream', t => {
+  t.throws(() => {
+    redMaple({
+      input: 1,
+      output: concat(noop),
+      map: noop,
+      reduce: noop
+    })
+  }, /Input must be a readable stream, named input pipe or file path/)
+  t.end()
+})
+
+tap.test('reject invalid output stream', t => {
+  const inputs = numbers.map(value => ({ value }))
+  t.throws(() => {
+    redMaple({
+      input: intoStream(inputs.map(v => JSON.stringify(v)).join('\n')),
+      output: 2,
+      map: noop,
+      reduce: noop
+    })
+  }, /Output must be a writable stream, named output pipe or file path/)
+  t.end()
 })
